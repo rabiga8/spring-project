66448ad7587413249eccd28d594f304ae907ad04
rabiga <49576033+rabiga8@users.noreply.github.com>
2024-04-15 19:38:22 -0400

    Update SpringProjectApplication.java - fix feature 2

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -9,6 +9,7 @@ public class SpringProjectApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(SpringProjectApplication.class, args);
 		System.out.println("Hello, Group Project is running!");
+		System.out.println("Fix feature 2");
 	}
 
 }
