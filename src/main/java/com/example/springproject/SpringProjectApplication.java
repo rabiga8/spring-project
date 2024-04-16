effb0b91c7a1f8f7c32cd86794050ccbf419e62d
rabiga <49576033+rabiga8@users.noreply.github.com>
2024-04-15 20:11:55 -0400

    Update SpringProjectApplication.java - Bug fix

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -9,6 +9,8 @@ public class SpringProjectApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(SpringProjectApplication.class, args);
 		System.out.println("Hello, Group Project is running!");
+
+		System.out.println("Bug-fix example");
 	}
 
 }
