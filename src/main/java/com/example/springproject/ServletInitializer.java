63f0c7b1b7fc9628dd23cdf7246b4afa7e79e32f
rabiga <49576033+rabiga8@users.noreply.github.com>
2024-04-16 23:11:15 -0400

    Update ServletInitializer.java

--- src/main/java/com/example/springproject/ServletInitializer.java
+++ src/main/java/com/example/springproject/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
       @Override
       protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-              return application.sources(DemoApplication.class); // Replace DemoApplication with your main class
+              return application.sources(SpringProjectApplication.class); // Replace DemoApplication with your main class
       }
 
 }
